ef96db5e5675fe3ce69d564ef5a8d0cdfe96d844
Nick Hamze <me@nickhamze.com>
2026-06-24 15:51:48 -0500

    Regenerate simplified blueprint logos

--- scripts/render-brand-assets.sh
+++ scripts/render-brand-assets.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-rsvg-convert -w 1200 -h 347 assets/lawn-care/greenstripe-logo.svg -o assets/lawn-care/greenstripe-logo.png
+rsvg-convert -w 1200 -h 260 assets/lawn-care/greenstripe-logo.svg -o assets/lawn-care/greenstripe-logo.png
 rsvg-convert -w 512 -h 512 assets/lawn-care/greenstripe-favicon.svg -o assets/lawn-care/greenstripe-favicon.png
-rsvg-convert -w 1200 -h 300 assets/pressure-washing/brightjet-logo.svg -o assets/pressure-washing/brightjet-logo.png
+rsvg-convert -w 1200 -h 260 assets/pressure-washing/brightjet-logo.svg -o assets/pressure-washing/brightjet-logo.png
 rsvg-convert -w 512 -h 512 assets/pressure-washing/brightjet-favicon.svg -o assets/pressure-washing/brightjet-favicon.png
